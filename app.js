d2951dca08127e851b0518836e3b7d8c4ac3ca47
Giorgos Logiotatidis <seadog@sealabs.net>
2013-10-21 15:19:09 +0300

    Working simple bot.
    Type !bug to one of the channels the bot sits and get a list of bugs
    with 'mentor' in their whiteboard for the Component related to that
    channel.

--- app.js
+++ app.js
@@ -3,42 +3,51 @@
 var IRC = require('irc');
 var bz = require('bz');
 var nconf = require('nconf');
-var ircChannels = require('./irc_channels.json');
+var express = require('express');
+var app = express();
 
-nconf.defaults({
-  nick: 'contributor-helper',
+nconf.file({ file: './local.json'}).defaults({
+    PORT: 3000,
+    nick: 'contributor-helper',
+    dev: false
 }).argv().env();
 
 var config = nconf.get();
 
+var ircChannels = require('./irc_channels.json');
+if ( config.dev ) {
+  ircChannels = require('./irc_channels_dev.json');
+}
+
+
 var ircClient = new IRC.Client('irc.mozilla.org', config.nick, {
-  secure: true,
-  port: 6697,
-  userName: config.nick,
-  realName: 'The friendly contributor helper',
-  channels: Object.keys(ircChannels)
-})
+    secure: true,
+    port: 6697,
+    userName: config.nick,
+    realName: 'The friendly contributor helper',
+    channels: Object.keys(ircChannels)
+});
+
 
 var bzClient = bz.createClient({
-  url: 'https://api-dev.bugzilla.mozilla.org/latest/'
+    url: 'https://api-dev.bugzilla.mozilla.org/latest/'
 });
 
 
 function fetchBugs(channel, product, component) {
-  var searchTerms = {'status_whiteboard': 'mentor',
-                     'whiteboard_type': 'contains_all',
-                     'product': product,
-                     'component': component,
-                     'bug_status': 'NEW'}
+    var searchTerms = {'status_whiteboard': 'mentor',
+                       'whiteboard_type': 'contains_all',
+                       'product': product,
+                       'component': component,
+                       'bug_status': 'NEW'};
 
-  console.log(searchTerms);
-  bzClient.searchBugs(searchTerms, function(errors, bugs) {
-    var bug;
-    for (var index in bugs) {
-      bug = bugs[index];
-      ircClient.say(channel, 'bug ' + bug.id);
-    }
-  });
+    bzClient.searchBugs(searchTerms, function   (errors, bugs) {
+        var bug, index;
+        for (index in bugs) {
+            bug = bugs[index];
+            ircClient.say(channel, 'bug ' + bug.id);
+        }
+    });
 }
 
 // via https://github.com/mythmon/standup-irc/blob/master/standup-irc.js
@@ -50,9 +59,25 @@ ircClient.on('registrered', function(message) {
     console.info('Using nickname: ' + config.realNick);
 });
 
+
 ircClient.on('message', function(user, channel, message) {
-  if (message === '!bug') {
-    ircClient.say(channel, 'Looking for bugs...');
-    fetchBugs(channel, ircChannels[channel].product, ircChannels[channel].component);
-  }
+    if (message === '!bug') {
+        ircClient.say(channel, 'Looking for bugs...');
+        fetchBugs(channel, ircChannels[channel].product, ircChannels[channel].component);
+    }
 });
+
+
+/// Express
+app.use(express.bodyParser());
+var data = 'Empty';
+
+app.get('/', function (request, response) {
+    response.send(data);
+});
+
+
+if (!module.parent) {
+    app.listen(config.PORT);
+    console.log('Express server listening on port ' + config.PORT);
+}
